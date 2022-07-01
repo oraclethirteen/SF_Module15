365305e28bfd6f70c32e4703a3933d10f6166d48
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-07-01 16:22:14 +0300

    Add project files.

---
+++ SF_Module15/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace SF_Module15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
